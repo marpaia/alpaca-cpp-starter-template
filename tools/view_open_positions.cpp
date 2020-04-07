e83e8b9f58c4e84e9c42d6de1e5e7b7ae7e0eae7
Mike Arpaia <mike@arpaia.co>
2020-04-07 10:43:07 -0600

    Move example to a tools/ directory

--- tools/view_open_positions.cpp
+++ tools/view_open_positions.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
 #include "alpaca/alpaca.h"
+#include "gflags/gflags.h"
+#include "glog/logging.h"
 
 int main(int argc, char* argv[]) {
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  FLAGS_logtostderr = 1;
+  FLAGS_minloglevel = 2;
+
+  // Parse the configuration from the environment
   auto env = alpaca::Environment();
   if (auto status = env.parse(); !status.ok()) {
     std::cerr << "Error parsing config from environment: " << status.getMessage() << std::endl;
     return status.getCode();
   }
-  auto client = alpaca::Client(env);
 
-  // Get our position in AAPL.
-  auto get_position_response = client.getPosition("AAPL");
-  if (auto status = get_position_response.first; status.ok()) {
-    auto position = get_position_response.second;
-    std::cout << "Apple position: " << position.qty << " shares." << std::endl;
-  } else {
-    std::cout << "No AAPL position.";
-  }
+  // Create an API client object
+  auto client = alpaca::Client(env);
 
   // Get a list of all of our positions.
   auto get_positions_response = client.getPositions();
@@ -26,8 +27,12 @@ int main(int argc, char* argv[]) {
     return status.getCode();
   }
   auto positions = get_positions_response.second;
+
+  // Print a summary of the currently held positions
+  std::cout << "Found " << positions.size() << " positions" << std::endl;
   for (const auto& position : positions) {
-    std::cout << position.qty << " shares in " << position.symbol << std::endl;
+    std::cout << "  - " << position.qty << " shares in " << position.symbol << " worth $" << position.market_value
+              << " each" << std::endl;
   }
 
   return 0;
